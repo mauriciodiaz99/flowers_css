af60f63c0839c51481cd9cd085b0358eef2b7ebd
mauriciodiaz99 <mauricioantoniodiazmartinez99@gmail.com>
2023-11-06 23:30:15 -0600

    Validacion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
 onload = () =>{
-    document.body.classList.remove("container");
+    verificarClave();
 };
+
+async function verificarClave() {
+    debugger;
+    const { value: password } = await Swal.fire({
+      title: "Enter your password",
+      input: "password",
+      inputLabel: "Password",
+      inputPlaceholder: "Enter your password",
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      inputAttributes: {
+        maxlength: "10",
+        autocapitalize: "off",
+        autocorrect: "off"
+      },
+      inputValidator: (value) => {
+        return new Promise((resolve) => {
+          if (value === "") {
+            resolve("Enter your password!");
+          } else if (value != 'Edward02') {
+            resolve("Incorrect password!");
+          } else {
+            resolve();
+            document.body.classList.remove("container");
+            const name = document.getElementById('nombre');
+            name.style.display = 'block';
+          }
+        });
+      }
+    });
+}
