edac56801671ef46a1de933786892a02e47eda14
mauriciodiaz99 <mauricioantoniodiazmartinez99@gmail.com>
2023-11-29 18:57:10 -0600

    e

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
 onload = () =>{
+
+    netlifyIdentity.open();
+debugger;
+const user = netlifyIdentity.currentUser();
+
+// Bind to events
+netlifyIdentity.on('init', user => console.log('init', user));
+netlifyIdentity.on('login', user => console.log('login', user));
+netlifyIdentity.on('logout', () => console.log('Logged out'));
+netlifyIdentity.on('error', err => console.error('Error', err));
+netlifyIdentity.on('open', () => console.log('Widget opened'));
+netlifyIdentity.on('close', () => console.log('Widget closed'));
+
+// Unbind from events
+netlifyIdentity.off('login'); // to unbind all registered handlers
+
+// Close the modal
+netlifyIdentity.close();
+
+// Log out the user
+netlifyIdentity.logout();
+
+// Refresh the user's JWT
+// Call in on('login') handler to ensure token refreshed after it expires (1hr)  
+// Note: this method returns a promise.
+netlifyIdentity.refresh().then((jwt)=>console.log(jwt))
+
+// Change language
+netlifyIdentity.setLocale('en');
+
     verificarClave();
 };
 
@@ -20,7 +50,7 @@ async function verificarClave() {
         return new Promise((resolve) => {
           if (value === "") {
             resolve("Enter your password!");
-          } else if (value != 'Edward01') {
+          } else if (value != 'elsa') {
             resolve("Incorrect password!");
           } else {
             resolve();
